5e09480e3178012fc63578f7b539d90c97662aab
egpivo <egpivo@gmail.com>
2024-03-28 11:38:23 +0800

    CHG: refine conda env scripts

--- envs/conda/build_conda_env.sh
+++ envs/conda/build_conda_env.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 #
-# Build Conda Env
+# Build Conda Environment
 #
-# - Parameters
-#    - Optional
-#       - -c/--conda_env: conda env name; Default: `sssd`
-# - Examples
-#       1. Default conda env: sssd
-#           - ./build_conda_env.sh
-#       2. New conda env (will automatically install empty conda env if necessary)
-#           - ./build_conda_env.sh -c sssd
+# This script builds a Conda environment based on the provided name or the default 'sssd'.
 #
-# - Caveat
-#    - If you don't have `realpath` on Mac, please install via `brew install coreutils`
+# Parameters:
+#   -c, --conda_env       Specifies the name of the Conda environment (default: 'sssd')
 #
+# Examples:
+#   1. Build the default Conda environment 'sssd':
+#       ./build_conda_env.sh
+#
+#   2. Build a specific Conda environment 'my_env':
+#       ./build_conda_env.sh -c my_env
+#
+# Note:
+#   - If 'realpath' is not available on macOS, install it via 'brew install coreutils'.
+#
+
+# Directory of the script
 CONDA_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set -a
 source "${CONDA_DIR}/conda_env_info.sh"
@@ -22,66 +27,60 @@ source "${COLOR_MAP_PATH}"
 source "${EXIT_CODE_PATH}"
 set +a
 
+# Default values for options
+CONDA_ENV="sssd"
 
-for ARG in "$@"; do
-  shift
-  case "${ARG}" in
-  "--conda_env") set -- "$@" "-c" ;;
-  *) set -- "$@" "${ARG}" ;;
-  esac
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c|--conda_env)
+            CONDA_ENV="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid option: $1"
+            exit 1
+            ;;
+    esac
 done
 
-while getopts "d:t:c:*" OPT; do
-  case "${OPT}" in
-  c)
-    CONDA_ENV="${OPTARG}"
-    ;;
-  *) ;;
-
-  esac
-done
-
-
+# Function to build the Conda environment
 build() {
-  local CONDA_ENV=$1
+    local CONDA_ENV="$1"
 
-  if [ "x${CONDA_ENV}x" == "xx" ]; then
-    CONDA_ENV="sssd"
-  fi
-
-  CONDA_HOME=$(dirname $(dirname $(which conda)))
-
-  # Will return `CONDA_ENV_DIR`
-  echo -e "${FG_YELLOW}Checking Conda Env: '${CONDA_ENV}'${FG_RESET}"
-  find_conda_env_path "${CONDA_ENV}"
-  # Try to build the conda env if the error code is captured
-  if [ "$?" == "${ERROR_EXITCODE}" ]; then
-    PYTHON_VERSION="${DEFAULT_PYTHON_VERSION}"
-    retry_to_find_conda_env_path "${CONDA_ENV}" "${PYTHON_VERSION}"
+    # Check if the Conda environment exists
+    CONDA_ENV_DIR="$(find_conda_env_path "${CONDA_ENV}")"
     if [ "$?" == "${ERROR_EXITCODE}" ]; then
-      return "${ERROR_EXITCODE}"
+        PYTHON_VERSION="${DEFAULT_PYTHON_VERSION}"
+        retry_to_find_conda_env_path "${CONDA_ENV}" "${PYTHON_VERSION}"
+        if [ "$?" == "${ERROR_EXITCODE}" ]; then
+            return "${ERROR_EXITCODE}"
+        fi
     fi
-  fi
 
-  initialize_conda
-  source activate "${CONDA_ENV}"
-  echo -e "${FG_YELLOW}Activating conda env '${CONDA_ENV}'${FG_RESET}"
+    # Activate the Conda environment
+    initialize_conda
+    source activate "${CONDA_ENV}"
+    echo -e "${FG_YELLOW}Activating Conda environment '${CONDA_ENV}'${FG_RESET}"
 
-  # Check HDF5 dependency [TODO] wrap it to other function
-  if conda list | grep -q hdf5; then
-    echo -e "${FG_GREEN}hdf5 is installed.${FG_RESET}"
-  else
-    echo "${FG_RED}hdf5 is not installed.${FG_RESET}"
-    conda install -c conda-forge hdf5 -y
-  fi
+    # Check HDF5 dependency
+    if conda list | grep -q hdf5; then
+        echo -e "${FG_GREEN}HDF5 is installed.${FG_RESET}"
+    else
+        echo "${FG_RED}HDF5 is not installed.${FG_RESET}"
+        conda install -c conda-forge hdf5 -y
+    fi
 
-  # Installation
-  echo -e "${FG_YELLOW}Installing package ${FG_RESET}"
-  install_python_package "${PACKAGE_BASE_PATH}"
-  echo -e "${FG_GREEN}Updated package${FG_RESET}"
-  conda deactivate
+    # Install packages
+    echo -e "${FG_YELLOW}Installing packages${FG_RESET}"
+    install_python_package "${PACKAGE_BASE_PATH}"
+    echo -e "${FG_GREEN}Updated packages${FG_RESET}"
 
-  return "${SUCCESS_EXITCODE}"
+    # Deactivate the Conda environment
+    conda deactivate
+
+    return "${SUCCESS_EXITCODE}"
 }
 
+# Build the Conda environment
 build "${CONDA_ENV}"
---
+++ envs/conda/pack_conda_env.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# Pack Conda Environment
+#
+# This script packs a Conda environment into a zip file.
+#
+# Usage:
+#   pack_conda_env.sh [OPTIONS]
+#
+# Options:
+#   -c, --conda_env       Conda environment name (default: 'sssd')
+#   -t, --target_project_dir
+#                         Target project directory (default: current directory)
+#   -d, --destination     Destination directory for the packed environment zip file (default: current directory)
+#
+# Example:
+#   Pack the default Conda environment 'sssd':
+#       pack_conda_env.sh
+#
+#   Pack a specific Conda environment 'my_env' and save it to the 'packages' directory:
+#       pack_conda_env.sh -c my_env -d packages
+#
+
+# Directory of the script
+CONDA_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Load utilities and environment settings
+set -a
+source "${CONDA_DIR}/conda_env_info.sh"
+source "${CONDA_DIR}/utils.sh"
+source "${COLOR_MAP_PATH}"
+source "${EXIT_CODE_PATH}"
+set +a
+
+# Default values for options
+CONDA_ENV="sssd"
+TARGET_PROJECT_DIR="${PACKAGE_BASE_PATH}"
+DESTINATION="${PACKAGE_BASE_PATH}"
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c|--conda_env)
+            CONDA_ENV="$2"
+            shift 2
+            ;;
+        -t|--target_project_dir)
+            TARGET_PROJECT_DIR="$2"
+            shift 2
+            ;;
+        -d|--destination)
+            DESTINATION="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Resolve paths
+CONDA_ENV_DIR="${TARGET_PROJECT_DIR}/${CONDA_ENV}"
+
+# Function to pack Conda environment as a zip file
+pack_conda_env_zip() {
+    local CONDA_ENV="$1"
+    local DESTINATION="$2"
+    local CONDA_ENV_DIR="$3"
+
+    echo -e "${FG_YELLOW}Packing Conda environment '${CONDA_ENV}' as '${CONDA_ENV}.zip'${FG_RESET}"
+
+    # Navigate to the Conda environment directory
+    pushd "$CONDA_ENV_DIR" > /dev/null || exit 1
+
+    # Create the destination directory if it doesn't exist
+    mkdir -p "${DESTINATION}"
+
+    # Pack the Conda environment as a zip file
+    zip -urq "${CONDA_ENV}.zip" .
+
+    # Move the zip file to the destination directory
+    mv "${DESTINATION}/${CONDA_ENV}.zip" "${DESTINATION}"
+    echo -e "${FG_YELLOW}Moved '${DESTINATION}/${CONDA_ENV}.zip' to '${DESTINATION}'${FG_RESET}"
+
+    # Return to the original directory
+    popd > /dev/null || exit 1
+}
+
+# Pack the Conda environment
+pack_conda_env_zip "${CONDA_ENV}" "$DESTINATION" "$CONDA_ENV_DIR"
