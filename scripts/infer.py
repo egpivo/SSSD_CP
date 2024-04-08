a1ec31142341475489ceb690db3a6277d3c127e9
egpivo <egpivo@gmail.com>
2024-04-08 10:30:14 +0800

    CHG: refine the inference process by adding trials option

--- scripts/infer.py
+++ scripts/infer.py
@@ -36,11 +36,22 @@ def fetch_args() -> argparse.Namespace:
         default="max",
         help='Which checkpoint to use; assign a number or "max"',
     )
+    parser.add_argument(
+        "-trials",
+        "--trials",
+        type=int,
+        default=1,
+        help="Trials of inference. If replications > 1, only save imputation results",
+    )
     return parser.parse_args()
 
 
 def run_job(
-    config: dict, device: torch.device, num_samples: int, ckpt_iter: Union[str, int]
+    config: dict,
+    device: torch.device,
+    num_samples: int,
+    ckpt_iter: Union[str, int],
+    trials: int,
 ) -> None:
 
     testing_data = load_testing_data(
@@ -62,23 +73,34 @@ def run_job(
     if torch.cuda.device_count() > 0:
         net = nn.DataParallel(net)
 
-    mse = DiffusionGenerator(
-        net=net,
-        device=device,
-        diffusion_hyperparams=diffusion_hyperparams,
-        testing_data=testing_data,
-        local_path=local_path,
-        output_directory=config["gen_config"]["output_directory"],
-        ckpt_path=config["gen_config"]["ckpt_path"],
-        ckpt_iter=ckpt_iter,
-        num_samples=num_samples,
-        masking=config["train_config"]["masking"],
-        missing_k=config["train_config"]["missing_k"],
-        only_generate_missing=config["train_config"]["only_generate_missing"],
-    ).generate()
+    data_names = ["imputation", "original", "mask"]
 
-    LOGGER.info(f"Average MSE: {sum(mse) / len(mse)}")
-    LOGGER.info(display_current_time())
+    if trials > 1:
+        directory = 'config["gen_config"]["output_directory"]_{trial}'
+    else:
+        directory = 'config["gen_config"]["output_directory"]'
+
+    for trial in range(trials):
+        saved_data_names = data_names if trial == 0 else data_names[0]
+
+        mse = DiffusionGenerator(
+            net=net,
+            device=device,
+            diffusion_hyperparams=diffusion_hyperparams,
+            testing_data=testing_data,
+            local_path=local_path,
+            output_directory=directory.format(trial=trial) if trials > 1 else directory,
+            ckpt_path=config["gen_config"]["ckpt_path"],
+            ckpt_iter=ckpt_iter,
+            num_samples=num_samples,
+            masking=config["train_config"]["masking"],
+            missing_k=config["train_config"]["missing_k"],
+            only_generate_missing=config["train_config"]["only_generate_missing"],
+            saved_data_names=saved_data_names,
+        ).generate()
+
+        LOGGER.info(f"Average MSE: {sum(mse) / len(mse)}")
+        LOGGER.info(display_current_time())
 
 
 if __name__ == "__main__":
@@ -94,4 +116,4 @@ if __name__ == "__main__":
     with open(args.config) as f:
         config = json.load(f)
 
-    run_job(config, device, args.num_samples, args.ckpt_iter)
+    run_job(config, device, args.num_samples, args.ckpt_iter, args.trials)
