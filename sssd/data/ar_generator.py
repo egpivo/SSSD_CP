3fb59f68db25b31583aa8a4e67a1d50838688d30
egpivo <egpivo@gmail.com>
2024-04-09 16:30:00 +0800

    CHG: add standard deviation

--- sssd/data/ar_generator.py
+++ sssd/data/ar_generator.py
@@ -8,31 +8,44 @@ class ArDataGenerator:
     Generates autoregressive (AR) time series data.
 
     Args:
-        coefficients (array_like): Coefficients of the AR process, where |coefficient| < 1
+        coefficients (array_like): Coefficients of the AR process, where |coefficient| < 1.
         n_sample (int): Number of samples to generate.
+        std (float, optional): Standard deviation of the generated samples. Must be greater than zero. Defaults to 1.
         seed (int, optional): Seed for random number generation. Defaults to None.
 
     Examples
     --------
     >>> from sssd.data.ar_generator import ArDataGenerator
-    >>> ArDataGenerator([0.1, 0.2, 0.3], 3, seed=1).generate()
-    array([1.62434536, -0.44932188, -0.24823487])
+    >>> ArDataGenerator([0.1, 0.2, 0.3], n_sample=3, std=1, seed=1).generate()
+    array([1.62434536, -0.44932188, -0.2482348])
     """
 
     def __init__(
-        self, coefficients: array_like, n_sample: int, seed: int = None
+        self, coefficients: array_like, n_sample: int, std: float = 1, seed: int = None
     ) -> None:
         """
-        Initialize the AR data generator with given coefficients and number of samples.
+        Initialize the AR data generator with given coefficients, number of samples, and standard deviation.
+        """
+        self._validate_inputs(coefficients, std)
+
+        ar_parameters = [1] + [-1 * coefficient for coefficient in coefficients]
+        self.ar_process = ArmaProcess(ar_parameters, [1, 0])
+        self.n_sample = n_sample
+        self.std = std
+        self.seed = seed
+
+    @staticmethod
+    def _validate_inputs(coefficients: array_like, std: float) -> None:
+        """
+        Validate input coefficients and standard deviation.
         """
         # Check if absolute values of coefficients are less than one
         if any(abs(coefficient) >= 1 for coefficient in coefficients):
             raise ValueError("Absolute values of coefficients must be less than one.")
 
-        ar_parameters = [1] + [-1 * coefficient for coefficient in coefficients]
-        self.ar_process = ArmaProcess(ar_parameters, [1, 0])
-        self.n_sample = n_sample
-        self.seed = seed
+        # Check if std is greater than zero
+        if std <= 0:
+            raise ValueError("Standard deviation (std) must be greater than zero.")
 
     def generate(self) -> np.array:
         """
@@ -43,4 +56,4 @@ class ArDataGenerator:
         """
         if self.seed:
             np.random.seed(self.seed)
-        return self.ar_process.generate_sample(nsample=self.n_sample)
+        return self.ar_process.generate_sample(nsample=self.n_sample, scale=self.std)
--- tests/data/test_ar_generator.py
+++ tests/data/test_ar_generator.py
@@ -10,7 +10,7 @@ def test_generate():
     seed = 1
     expected_output = np.array([1.62434536, -0.44932188, -0.24823487])
 
-    generator = ArDataGenerator(coefficients, n_sample, seed)
+    generator = ArDataGenerator(coefficients=coefficients, n_sample=n_sample, seed=seed)
     output = generator.generate()
 
     try:
@@ -28,3 +28,25 @@ def test_generate_invalid_coefficients():
 
     with pytest.raises(ValueError):
         ArDataGenerator(coefficients, n_sample, seed)
+
+
+def test_generate_invalid_std():
+    coefficients = [0.1, 0.2, 0.3]
+    n_sample = 5
+    seed = 1
+    std = 0
+
+    with pytest.raises(ValueError):
+        ArDataGenerator(coefficients, n_sample, std=std, seed=seed)
+
+
+def test_generate_with_different_std():
+    coefficients = [0.1, 0.2, 0.3]
+    n_sample = 5
+    seed = 1
+    std = 0.5
+
+    generator = ArDataGenerator(coefficients, n_sample, std=std, seed=seed)
+    output = generator.generate()
+
+    assert len(output) == n_sample
