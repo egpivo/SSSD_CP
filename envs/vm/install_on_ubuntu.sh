b67b81480d6f5aa24c07ffcf9360c9fe7d66a200
egpivo <egpivo@gmail.com>
2024-04-23 17:52:38 +0800

    CHG: change the permission of docker

--- envs/vm/install_on_ubuntu.sh
+++ envs/vm/install_on_ubuntu.sh
@@ -13,16 +13,25 @@ install_docker_and_compose() {
     # Install Docker Compose
     sudo curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+
+    # Add current user to the docker group
+    sudo usermod -aG docker "$USER"
+    sudo chown "$USER":docker /var/run/docker.sock
 }
 
 install_nvidia_drivers() {
+    # References:
+    # - https://man.twcc.ai/@twccdocs/doc-vcs-main-zh/https%3A%2F%2Fman.twcc.ai%2F%40twccdocs%2Fhowto-vcs-install-nvidia-gpu-driver-zh
+    # - https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+    #
+
     # Install NVIDIA driver
     sudo apt update
     sudo DEBIAN_FRONTEND=noninteractive apt install -y nvidia-driver-535
 
     # Install CUDA
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-    sudo dpkg -i cuda-keyring_1.1-1_all.deb
+    sudo dpkg -i --force-all cuda-keyring_1.1-1_all.deb
     sudo apt update
     sudo DEBIAN_FRONTEND=noninteractive apt install -y cuda
 
@@ -35,13 +44,13 @@ install_nvidia_drivers() {
     # Configure NVIDIA Container Toolkit runtime for Docker
     sudo nvidia-ctk runtime configure --runtime=docker
 
-    # Download and install NVIDIA driver (Optional, if needed)
-    # wget https://us.download.nvidia.com/tesla/460.106.00/NVIDIA-Linux-x86_64-460.106.00.run
-    # sudo apt install -y linux-headers-$(uname -r)
+    # Download and install NVIDIA driver
+    wget https://us.download.nvidia.com/tesla/460.106.00/NVIDIA-Linux-x86_64-460.106.00.run
+    sudo apt install -y linux-headers-$(uname -r)
 
-    # Blacklist nouveau and set options (Optional, if needed)
-    # echo "blacklist nouveau" | sudo tee -a /etc/modprobe.d/blacklist.conf
-    # echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/blacklist.conf
+    # Blacklist nouveau and set options
+    echo "blacklist nouveau" | sudo tee -a /etc/modprobe.d/blacklist.conf
+    echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/blacklist.conf
 }
 
 main() {
