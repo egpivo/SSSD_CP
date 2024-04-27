aeb710037957c4b6885ab033d8e631715d2a8613
egpivo <egpivo@gmail.com>
2024-04-27 14:21:51 +0800

    CHG: refine scripts

--- envs/notebook/start_jupyter_lab.sh
+++ envs/notebook/start_jupyter_lab.sh
@@ -1,81 +1,54 @@
 #!/bin/bash
 #
-# Start a Jupyter notebook server with Kernel environment setting to connect Spark seamlessly
+# Start a Jupyter Lab server with the specified kernel environment and configure Spark and GPU settings.
+# Parameters:
+#    -k or --kernel_env: Specify the kernel environment name (default: optimus).
+#    -p or --port: Specify the port number for the Jupyter Lab server (default: 8501).
+#    -C or --rebuild_conda: Set the DOES_UPDATE_CONDA flag to TRUE to update the Conda environment.
 #
-# - Parameters
-#    - Optional
-#       - -k/--kernel_env: kernel env name; Default: optimus
-#       - -q/--queue: queue on Yarn; default: mkplalgo-dev
-#       - -x/--num_executors: number of executors; default: based on ${SPARK_CONF_DIR}
-#       - -m/--executor_memory: memory per executor; default: based on ${SPARK_CONF_DIR}
-#       - -p/--port: port for notebook server; default: 8000
-#       - -C/--rebuild_conda: rebuild the Cond environment by adding this tag
-#       - -G/--does_use_gpu: update the environment variables related to GPU driver directories
-#       - -E/--install ego: install ego in the Conda environment (only valid when the flag `--rebuild_conda` is added)
-# - Examples
-#       1. Default conda env: optimus
-#           - ./start_notebook_server.sh -K -C
-#       2. New conda env
-#           - ./start_notebook_server.sh -k temp -K -C
-#       3. Modify Spark resource
-#           - ./start_notebook_server.sh -x 20 -m 16g
-#       4. Default conda env: optimus with ego
-#           - ./start_notebook_server.sh -K -C -E
-# - Caveats
-#    - The kernel environment name should be identical to the name in Conda environment.
-#    - This requires that jupyter notebook service is installed in your environment.
 #
+
+# Set default values
+KERNEL_ENV="optimus"
+PORT="8501"
+DOES_UPDATE_CONDA="FALSE"
+
+# Parse command-line arguments
+while getopts "k:p:C" OPT; do
+  case "${OPT}" in
+    k) KERNEL_ENV="${OPTARG}" ;;
+    p) PORT="${OPTARG}" ;;
+    C) DOES_UPDATE_CONDA="TRUE" ;;
+    *) ;;
+  esac
+done
+
+# Set up environment
 NOTEBOOK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${NOTEBOOK_DIR}/utils.sh"
 PACKAGE_BASE_PATH="${NOTEBOOK_DIR}/../.."
 source "${PACKAGE_BASE_PATH}/bin/color_map.sh"
 source "${PACKAGE_BASE_PATH}/bin/exit_code.sh"
 
-
-for ARG in "$@"; do
-  shift
-  case "${ARG}" in
-  "--kernel_env") set -- "$@" "-k" ;;
-  "--port") set -- "$@" "-p" ;;
-  "--rebuild_conda") set -- "$@" "-C" ;;
-  *) set -- "$@" "${ARG}" ;;
-  esac
-done
-
-while getopts "k:p:C*" OPT; do
-  case "${OPT}" in
-  k)
-    KERNEL_ENV="${OPTARG}"
-    ;;
-  p)
-    PORT="${OPTARG}"
-    ;;
-  C)
-    DOES_UPDATE_CONDA="TRUE"
-    ;;
-  *) ;;
-  esac
-done
-
 start_jupyter_server() {
   local PORT="$1"
-  if [ "x${KERNEL_ENV}x" == "xx" ]; then
-    KERNEL_ENV="sssd"
+
+  if [ "${DOES_UPDATE_CONDA}" == "TRUE" ]; then
+    update_conda_env_path "${KERNEL_ENV}"
   fi
 
-  update_conda_env_path "${KERNEL_ENV}"
-  source activate "${KERNEL_ENV}"
-  is_jupyter_kernel_path_available "${KERNEL_ENV}"
+  conda activate "${KERNEL_ENV}"
+
+  check_kernel_availability "${KERNEL_ENV}"
   if [ "$?" == "${ERROR_EXITCODE}" ]; then
     set_jupyter_kernel_path "${KERNEL_ENV}"
   fi
-  update_gpu_env ${KERNEL_ENV}
+
+  update_gpu_env "${KERNEL_ENV}"
 
   jupyter lab --ip=0.0.0.0 --port "${PORT}" --no-browser --NotebookApp.token='' --NotebookApp.password='' --allow-root
+
   conda deactivate
 }
 
-if [ "x${PORT}x" == "xx" ]; then
-  PORT="8501"
-fi
-start_jupyter_server ${PORT}
+start_jupyter_server "${PORT}"
--- envs/notebook/utils.sh
+++ envs/notebook/utils.sh
@@ -4,63 +4,73 @@ source "${DIR}/../../bin/exit_code.sh"
 source "${DIR}/../../bin/color_map.sh"
 
 update_conda_env_path() {
-  # Will return `CONDA_ENV_DIR`
   local ENV_NAME=$1
+  local CONDA_BASE=$(conda info --base)
+  source "${CONDA_BASE}/etc/profile.d/conda.sh"
 
-  source $(conda info --base)/etc/profile.d/conda.sh
-  IFS=' ' read -r -a CONDA_INFO <<<"$(conda env list | grep "${ENV_NAME}")"
-  AVAILABLE_ENV_NAME="${CONDA_INFO[0]}"
-
-  if [[ "x${AVAILABLE_ENV_NAME}x" != "x${ENV_NAME}x" ]]; then
+  # Check if the environment exists
+  if ! conda env list | grep -q "^${ENV_NAME}"; then
     echo -e "${FG_RED}Conda env '${ENV_NAME}' is not available${FG_RESET}"
     return "${ERROR_EXITCODE}"
   fi
 
-  . ${DIR}/../conda/build_conda_env.sh -c "${ENV_NAME}"
-  source activate "${ENV_NAME}"
-  poetry install --with notebook
+  # Activate the environment
+  conda activate "${ENV_NAME}"
+
+  # Install dependencies with Poetry
+  . "${DIR}/../conda/build_conda_env.sh" -c "${ENV_NAME}"
+  poetry install --extras notebook --no-root
+
+  # Deactivate the environment
   conda deactivate
 }
 
-is_jupyter_kernel_path_available() {
-  # Will return `KERNEL_DIR` (assume conda env is activated)
-  KERNEL_NAME=$1
-  IFS=' ' read -r -a KERNEL_INFO <<<"$(jupyter kernelspec list | grep "${KERNEL_NAME}")"
-  AVAILABLE_KERNEL="${KERNEL_INFO[0]}"
-  if [[ "x${AVAILABLE_KERNEL}x" != "x${KERNEL_NAME}x" ]]; then
-    echo -e "${FG_RED} '${KERNEL_NAME}' is not available${FG_RESET}"
-    return ${ERROR_EXITCODE}
+check_kernel_availability() {
+  local KERNEL_NAME=$1
+  local KERNEL_PATH=$(jupyter kernelspec list | grep -o "^${KERNEL_NAME} .*" | cut -d' ' -f2)
+
+  if [ -z "${KERNEL_PATH}" ]; then
+    echo -e "${FG_RED}Kernel '${KERNEL_NAME}' is not available${FG_RESET}"
+    return "${ERROR_EXITCODE}"
   else
-    return ${SUCCESS_EXITCODE}
+    return "${SUCCESS_EXITCODE}"
   fi
 }
 
 set_jupyter_kernel_path() {
-  # Will return `KERNEL_DIR` (assume conda env is activated)
-  KERNEL_NAME=$1
-  is_jupyter_kernel_path_available "${KERNEL_NAME}"
+  local KERNEL_NAME=$1
+  local KERNEL_PATH
+
+  check_kernel_availability "${KERNEL_NAME}"
   if [ "$?" == "${ERROR_EXITCODE}" ]; then
-    echo -e "${FG_RED} Install kernel '${KERNEL_NAME}' now ${FG_RESET}"
+    echo -e "${FG_RED}Installing kernel '${KERNEL_NAME}'${FG_RESET}"
     ipython kernel install --name "${KERNEL_NAME}" --user
   fi
 
-  # Exclude the pattern `python3` to avoid fetching the wrong path
-  IFS=' ' read -r -a KERNEL_INFO <<<"$(jupyter kernelspec list | grep "${KERNEL_NAME}" | grep -v "python3")"
-  if [ "x${KERNEL_INFO[1]}x" == "x*x" ]; then
-    KERNEL_DIR="${KERNEL_INFO[2]}"
+  KERNEL_PATH=$(jupyter kernelspec list | grep -o "^${KERNEL_NAME} .*" | cut -d' ' -f2)
+
+  if [ -n "${KERNEL_PATH}" ]; then
+    KERNEL_DIR="${KERNEL_PATH}"
   else
-    KERNEL_DIR="${KERNEL_INFO[1]}"
+    echo -e "${FG_RED}Failed to get kernel path for '${KERNEL_NAME}'${FG_RESET}"
+    return "${ERROR_EXITCODE}"
   fi
 }
 
 update_gpu_env() {
-  # This function is used before making connection between Tensorflow and GPUs
   local CONDA_ENV="$1"
-  # will update `CONDA_ENV_DIR`
-  find_conda_env_path "${CONDA_ENV}"
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"${CONDA_ENV_DIR}/lib"
+  local CONDA_ENV_DIR
 
-  export CUDA_HOME=/usr/local/cuda
-  export PATH=${CUDA_HOME}/bin:${PATH}
-  export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+  # Find the path to the Conda environment
+  find_conda_env_path "${CONDA_ENV}"
+  CONDA_ENV_DIR="${CONDA_ENV_PATH}"
+
+  # Update LD_LIBRARY_PATH for the Conda environment
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CONDA_ENV_DIR}/lib"
+
+  # Set CUDA environment variables
+  local CUDA_HOME="/usr/local/cuda"
+  export CUDA_HOME
+  export PATH="${CUDA_HOME}/bin:${PATH}"
+  export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
 }
