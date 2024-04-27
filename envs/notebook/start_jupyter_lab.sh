050cf1d4f574c9bfdc5dd10a4bfada84b892597b
egpivo <egpivo@gmail.com>
2024-04-27 15:20:56 +0800

    FIX: correct the argument parsing

--- envs/notebook/start_jupyter_lab.sh
+++ envs/notebook/start_jupyter_lab.sh
@@ -2,11 +2,11 @@
 #
 # Start a Jupyter Lab server with the specified kernel environment and configure Spark and GPU settings.
 # Parameters:
-#    -k or --kernel_env: Specify the kernel environment name (default: sssd).
-#    -p or --port: Specify the port number for the Jupyter Lab server (default: 8501).
-#    -C or --rebuild_conda: Set the DOES_UPDATE_CONDA flag to TRUE to update the Conda environment.
+#    -k|--kernel_env: Specify the kernel environment name (default: sssd).
+#    -p|--port: Specify the port number for the Jupyter Lab server (default: 8501).
+#    -C|--update_conda: Set the DOES_UPDATE_CONDA flag to TRUE to update the Conda environment.
 #
-#
+
 
 # Set default values
 KERNEL_ENV="sssd"
@@ -14,22 +14,35 @@ PORT="8501"
 DOES_UPDATE_CONDA="FALSE"
 
 # Parse command-line arguments
-while getopts "k:p:C" OPT; do
-  case "${OPT}" in
-    k) KERNEL_ENV="${OPTARG}" ;;
-    p) PORT="${OPTARG}" ;;
-    C) DOES_UPDATE_CONDA="TRUE" ;;
-    *) ;;
-  esac
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -k|--kernel_env)
+            KERNEL_ENV="$2"
+            shift 2
+            ;;
+        -p|--port)
+            PORT="$2"
+            shift 2
+            ;;
+        -C|--update_conda)
+            DOES_UPDATE_CONDA="TRUE"
+            shift
+            ;;
+        *)
+            echo "Invalid option: $1"
+            exit 1
+            ;;
+    esac
 done
-
 # Set up environment
 NOTEBOOK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${NOTEBOOK_DIR}/utils.sh"
 PACKAGE_BASE_PATH="${NOTEBOOK_DIR}/../.."
 source "${PACKAGE_BASE_PATH}/bin/color_map.sh"
 source "${PACKAGE_BASE_PATH}/bin/exit_code.sh"
+local CONDA_BASE=$(conda info --base)
 source "${CONDA_BASE}/etc/profile.d/conda.sh"
+
 
 start_jupyter_server() {
   local PORT="$1"
