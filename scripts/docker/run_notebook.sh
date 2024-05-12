dc8eb7f5d8342ecbe508e17fed3cd15ebc2f4eba
egpivo <egpivo@gmail.com>
2024-05-12 13:36:50 +0800

    FIX: correct the path

--- scripts/docker/run_notebook.sh
+++ scripts/docker/run_notebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-${DOCKER_DIR}/../../envs/jputer/start_jupyter_lab.sh \
+${DOCKER_DIR}/../../envs/jupyter/start_jupyter_lab.sh \
   --port ${PORT}
