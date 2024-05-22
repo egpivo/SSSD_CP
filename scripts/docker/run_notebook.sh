eed665380f62e800c204660da60b93aac29eff0b
egpivo <egpivo@gmail.com>
2024-05-22 14:58:56 +0800

    CHG: remove redundant lines

--- scripts/docker/run_notebook.sh
+++ scripts/docker/run_notebook.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-conda activate sssd
-poetry install --no-root
-
 ${DOCKER_DIR}/../../envs/jupyter/start_jupyter_lab.sh \
   --port ${PORT} \
   --does_update_conda
