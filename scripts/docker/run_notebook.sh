0dd3540eeae4b850d74459a11becf1d9679d2d8f
egpivo <egpivo@gmail.com>
2024-05-22 14:44:40 +0800

    CHG: rollback the installation

--- scripts/docker/run_notebook.sh
+++ scripts/docker/run_notebook.sh
@@ -2,7 +2,7 @@
 DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 conda activate sssd
-poetry install --extras notebook
+poetry install --no-root
 
 ${DOCKER_DIR}/../../envs/jupyter/start_jupyter_lab.sh \
   --port ${PORT}
