4a4256cda79a0fb26685ba51a66ce144039a1616
egpivo <egpivo@gmail.com>
2024-05-24 22:28:50 +0800

    CHG: add mape metric

--- sssd/inference/generator.py
+++ sssd/inference/generator.py
@@ -4,7 +4,7 @@ from typing import Dict, Iterable, Optional, Union
 
 import numpy as np
 import torch
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error
 from torch.utils.data import DataLoader
 
 from sssd.core.model_specs import MASK_FN
@@ -125,6 +125,7 @@ class DiffusionGenerator:
     def generate(self) -> list:
         """Generate samples using the given neural network model."""
         all_mses = []
+        all_mapes = []
         for index, (batch,) in enumerate(self.dataloader):
             batch = batch.to(self.device)
             mask = self._update_mask(batch)
@@ -152,7 +153,11 @@ class DiffusionGenerator:
             mse = mean_squared_error(
                 generated_series[~mask.astype(bool)], batch[~mask.astype(bool)]
             )
+            mape = mean_absolute_percentage_error(
+                generated_series[~mask.astype(bool)], batch[~mask.astype(bool)]
+            )
             all_mses.append(mse)
+            all_mapes.append(mape)
             results = {
                 "imputation": generated_series,
                 "original": batch,
@@ -160,4 +165,4 @@ class DiffusionGenerator:
             }
             self._save_data(results, index)
 
-        return all_mses
+        return all_mses, all_mapes
